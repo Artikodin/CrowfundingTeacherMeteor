860b458f1efb8b12e5a1a127cecd41d3b24b1dcc
Artikodin <artium.wasse@gmail.com>
2017-12-06 15:32:03 +0100

    ajout de react

--- client/main.js
+++ client/main.js
@@ -1,4 +1,9 @@
-// Client entry point, imports all client code
-
-import '/imports/startup/client';
-import '/imports/startup/both';
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+ 
+import App from '../imports/ui/App';
+ 
+Meteor.startup(() => {
+  render(<App />, document.getElementById('app'));
+});
